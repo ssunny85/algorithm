e8a3e15888ba0fe8bc692bf97145f07b22ea4f30
ssunny <ssssunny85@gmail.com>
2020-02-14 19:09:58 +0900

    Add Programmers 12973.js

---
+++ programmers/12973.js
@@ -0,0 +1,27 @@
+function solution(s) {
+  let answer = null;
+  let arr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (!arr.length) {
+      arr.push(s[i]);
+    } else {
+      if(s[i] === arr[arr.length - 1]) {
+        arr.pop();
+      } else {
+        arr.push(s[i]);
+      }
+    }
+  }
+
+  if(arr.length > 0) {
+    answer = 0;
+  } else {
+    answer = 1;
+  }
+  console.log(answer);
+  return answer;
+}
+
+console.log(solution('baabaa'));
+console.log(solution('cdcd'));
